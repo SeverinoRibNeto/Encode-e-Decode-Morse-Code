52561a987afb8defbfadadd1386cbeb3d0afdd08
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-02-01 10:55:41 -0300

    Deleted screen.py and move to main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from tkinter import *
+from tkinter import ttk
+
 import encodeMorse
+
+inicialScreen = Tk()
+inicialScreen.title("Encode and Decode Morse Code")
+
+frame = ttk.Frame(inicialScreen, padding=20)
+frame.grid()
+ttk.Label(frame,
+          text="Encode and Decode Morse Code",
+          font=('Arial Bold', 20)
+          ).grid(columns=2, row=0)
+
+Label(frame, text="Use this app to generate a Morse code, decode and listen a sound of code",
+      font=('Arial normal', 12)).grid(columns=2, row=1, padx=10, pady=10)
+infoText = Label(frame, text="Write your message to code to Morse code:").grid(
+    columns=1, row=2, padx=10, pady=10)
+textToConvert = Entry(frame, width=100).grid(column=1, row=2, padx=20, pady=20)
+
+inicialScreen.mainloop()
--- screen.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-
-inicialScreen = Tk()
-inicialScreen.title("Encode and Decode Morse Code")
-
-inicialScreen.mainloop()
